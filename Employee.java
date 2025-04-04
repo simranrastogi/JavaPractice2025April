4fe9579b018c02967bfbe9e71c67baf6d65c2aea
Simran <vikas42329@gmail.com>
2025-04-04 17:26:05 +0530

    Employee class new code added

--- Employee.java
+++ Employee.java
@@ -2,5 +2,7 @@ public class Employee {
     public static void main(String[] args) {
         System.out.println("Heloo empl");
         System.out.println("slddfjkdf");
+        System.out.println("7896hsuqw");
+
     }
 }
